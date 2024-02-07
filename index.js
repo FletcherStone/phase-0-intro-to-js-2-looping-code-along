4009c04cb0ca31affee76ca612dcaa152452fee5
Fletcher Stone <Fletcher.w.stone@gmail.com>
2024-02-07 18:05:24 -0500

    Completes lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solutions in this file
+// for (let age=30; age <40; age++){
+//     console.log(`Im ${age} years old. Happy birthday to me!`)
+//     debugger;
+// }
+
+
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     debugger;
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
+
+//New Funciton
+const cards = ["Jim","Rich","Frank"]
+
+function writeCards(names, event) {
+    const giftMessages =[]
+    for(let i=0; i<names.length; i++){
+        const messages= `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        giftMessages.push(messages)
+        debugger;
+    }   
+    return giftMessages
+}
+
+writeCards(["Fletcher","Steve","Matt"], "birthday")
+
+function countDown(startingNumber){
+    let i= startingNumber
+    while (i>=0){
+        console.log(i)
+        i--
+    }
+}
